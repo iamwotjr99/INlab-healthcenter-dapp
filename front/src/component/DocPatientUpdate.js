6555538c49a70770cb225b01bdd4d2b101f7f310
iamwotjr99 <iamwotjr@gmail.com>
2022-05-18 18:00:56 +0900

    Update patient data

---
+++ front/src/component/DocPatientUpdate.js
@@ -0,0 +1,92 @@
+import { useState, useEffect } from 'react';
+import { Button, Form } from 'react-bootstrap';
+
+function DocPatientUpdate({ patientAddr, account, contract }) {
+    const [formData, setFormData] = useState({
+        address: "",
+        name: "",
+        age: "",
+        weight: "",
+        height: "",
+        symptom: "",
+        description: "",
+    });
+
+    useEffect(() => {
+        async function getPatientData() {
+            const result = await contract.methods.readPatientData(patientAddr).call();
+            setFormData({
+                ...formData,
+                address: result.patientAddr,
+                name: result.patientName,
+                age: result.patientAge,
+                weight: result.patientWeight,
+                height: result.patientHeight,
+                symptom: result.symptom,
+                description: result.description,
+            })
+        }
+        getPatientData();
+
+    }, [])
+
+    const changeHandler = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const btnUpdateHandler = async () => {
+        await contract.methods.updatePatientData(patientAddr, formData.name,
+            formData.age, formData.weight, formData.height, formData.symptom,
+            formData.description).send({from: account}).then(console.log);
+    }
+
+    return (
+        <div className="doctor_patient_update">
+            <Form>
+                <Form.Group className='mb-3' controlId="address">
+                    <Form.Label>Patient address</Form.Label>
+                    <Form.Control type="text" name="address" value={formData.address} onChange={changeHandler}/>
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId="name">
+                    <Form.Label>Patient name</Form.Label>
+                    <Form.Control type="text" name="name" value={formData.name} onChange={changeHandler}/>
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId="age">
+                    <Form.Label>Patient age</Form.Label>
+                    <Form.Control type="text" name="age" value={formData.age} onChange={changeHandler}/>
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId="weight">
+                    <Form.Label>Patient weight</Form.Label>
+                    <Form.Control type="text" name="weight" value={formData.weight} onChange={changeHandler}/>
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId="height">
+                    <Form.Label>Patient height</Form.Label>
+                    <Form.Control type="text" name="height" value={formData.height} onChange={changeHandler}/>
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId="symptom">
+                    <Form.Label>Patient symptom</Form.Label>
+                    <Form.Control type="text" name="symptom" value={formData.symptom} onChange={changeHandler}/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="desciption">
+                    <Form.Label>Patient description</Form.Label>
+                    <Form.Control type="text" name="description" as="textarea" 
+                    value={formData.description}
+                    rows={3} onChange={changeHandler}/>
+                </Form.Group>
+                <Button variant='warning'
+                    onClick={btnUpdateHandler}>Update</Button>
+            </Form>
+        </div>
+    )
+}
+
+export default DocPatientUpdate;
--- front/src/component/DoctorPatientList.js
+++ front/src/component/DoctorPatientList.js
@@ -1,13 +1,17 @@
 import Web3 from 'web3';
 import { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
+
 import {HEALTH_CARE_ABI, CONTRACT_ADDRESS} from '../abi/HealthCareABI';
+import DocPatientUpdate from './DocPatientUpdate';
 function DoctorPatientList() {
 
     const [patienList, setPaientList] = useState([]);
     const [contract, setContract] = useState();
     const [account , setAccount] = useState();
     const [count, setCount] = useState(0);
+    const [toggle, setToggle] = useState(false);
+    const [patientAddr, setPatientAddr] = useState();
 
     useEffect(() => {
         async function init() {
@@ -27,22 +31,35 @@ function DoctorPatientList() {
     }, [count])
 
     const btnDeleteHandler = async (e) => {
-        const patientAddr = e.target.value;
+        setPatientAddr(e.target.value);
         console.log(patientAddr);
         await contract.methods.deletePatientData(patientAddr).send({
             from: account
         }).then(setCount(count + 1));
     }
+
+    const btnUpdateToggle = (e) => {
+        setPatientAddr(e.target.value);
+        setToggle(true);
+    }
+
     return (
         <div className="doctor_patient_list">
-            <h1>Patient List</h1>
-            {patienList.map((item, index) => {
-                return (<li key={index}>{item} 
-                <Button 
-                    value={item} 
-                    onClick={btnDeleteHandler}>delete
-                </Button></li>)
-            })}
+            {toggle ? <DocPatientUpdate patientAddr={patientAddr} account={account} contract={contract} /> : 
+                <div>
+                    <h1>Patient List</h1>
+                    {patienList.map((item, index) => {
+                        return (<li key={index}>{item} 
+                        <Button
+                            value={item}
+                            onClick={btnUpdateToggle}>Update</Button>
+                        <Button 
+                            value={item}
+                            onClick={btnDeleteHandler}>Delete
+                        </Button></li>)
+                    })}
+                </div>
+            }
         </div>
     )
 }
