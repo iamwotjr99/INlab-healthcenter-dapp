efff9dc202dae43af02fd36561a153ac28861f22
iamwotjr99 <iamwotjr@gmail.com>
2022-06-25 15:56:35 +0900

    console get patient list in doctor

--- front/src/component/user/DoctorPList.js
+++ front/src/component/user/DoctorPList.js
@@ -1,4 +1,18 @@
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 function DoctorPList() {
+    const {id} = useParams();
+    const BASE_URL = "http://203.247.240.226:8080/fhir"
+
+    async function getPatients() {
+        await axios.get(`${BASE_URL}/Patient?general-practitioner=${id}`).then((res) => {
+            console.log(res.data.entry);
+        })
+    }
+    useEffect(() => {
+        getPatients();
+    }, [])
     return (
         <div className="doctor_pList">Patient List</div>
     )
