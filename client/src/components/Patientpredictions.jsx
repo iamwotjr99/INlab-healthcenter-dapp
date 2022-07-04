53a34287ad26688071322dbe959917c0a08cb34f
iamwotjr99 <iamwotjr@gmail.com>
2022-07-04 12:11:27 +0900

    integration

---
+++ client/src/components/Patientpredictions.jsx
@@ -0,0 +1,17 @@
+import '../css/PatientPrediction.css';
+
+function Patientpredictions() {
+
+  return (
+    <div className="structure">
+      <div className="UserDatalist"> user Data Choose</div>
+      <div className="UserResult"> Data Machine Learning result</div>
+    </div>
+      
+
+      
+  )
+}
+
+
+export default Patientpredictions;
--- front/src/component/HospitalSignUp.js
+++
@@ -1,7 +0,0 @@
-function HospitalSignUp() {
-    return (
-        <div className="hospital_sign_up "></div>
-    )
-}
-
-export default HospitalSignUp;
